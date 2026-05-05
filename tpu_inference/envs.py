05ade0f2cfb34a3bd9b3b59bcff517fdcc8e3564
Your Name <you@example.com>
2026-05-05 23:00:16 +0000

    [Qwen3.5] Using approx top k for expert selection
    Enabled by defaul. Can be disabled by MOE_APPROX_TOPK=False
    
    Performance: ~3% better throughput for 8k/1k 64 concurrency
    Eval: mmlu_pro overall stays the same 82.37
    
    For 8k/1k 64 concurrency
    before
    Output token throughput (tok/s):         820.26
    Total Token throughput (tok/s):          7394.97
    after
    Output token throughput (tok/s):         846.45
    Total Token throughput (tok/s):          7631.07

--- tpu_inference/envs.py
+++ tpu_inference/envs.py
@@ -51,6 +51,8 @@ if TYPE_CHECKING:
     TPU_OFFLOAD_SAVE_THREADS: int = 1
     TPU_OFFLOAD_BATCHED_SAVE: bool = False
     TPU_OFFLOAD_METRICS_LOG_INTERVAL: int = 5
+    MOE_APPROX_TOPK: bool = False
+    MOE_APPROX_TOPK_RECALL_TARGET: float | None = None
 
 
 def env_with_choices(
@@ -288,6 +290,15 @@ environment_variables: dict[str, Callable[[], Any]] = {
     # kv offload to dram: prometheus metrics log interval in seconds
     "TPU_OFFLOAD_METRICS_LOG_INTERVAL":
     lambda: int(os.getenv("TPU_OFFLOAD_METRICS_LOG_INTERVAL", "10")),
+    # MoE: whether to use approximate top-k for expert selection.
+    # Enabling this may speedup the expert selection at the risk of accuracy loss.
+    "MOE_APPROX_TOPK":
+    env_bool("MOE_APPROX_TOPK", default=False),
+    # MoE: the target recall rate for approximate top-k expert selection.
+    # A higher rate increases accuracy at the cost of slower speed.
+    # A lower rate can speedup expert selection at the risk of higher accuracy loss.
+    "MOE_APPROX_TOPK_RECALL_TARGET":
+    lambda: float(os.getenv("MOE_APPROX_TOPK_RECALL_TARGET", "0.9")),
 }
 
 
